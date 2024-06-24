c879e389a6b78ff617eb5d30dd9a5891f91c4320
miadjalili <jalilimiad@gmail.com>
2024-06-23 19:46:24 -0700

    Initial Commit

---
+++ SwiftUICryptoApp/SwiftUICryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICryptoAppApp.swift
+//  SwiftUICryptoApp
+//
+//  Created by Miad Jalili on 2024-06-23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUICryptoAppTests/SwiftUICryptoAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftUICryptoAppTests.swift
+//  SwiftUICryptoAppTests
+//
+//  Created by Miad Jalili on 2024-06-23.
+//
+
+import Testing
+
+struct SwiftUICryptoAppTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
