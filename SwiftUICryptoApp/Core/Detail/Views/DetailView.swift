7e4206ac51a9a83facae35a825d9cd0718bbba2c
miadjalili <jalilimiad@gmail.com>
2024-07-04 01:21:06 -0700

    Create a custom line chart to display historical price data

--- SwiftUICryptoApp/Core/Detail/Views/DetailView.swift
+++ SwiftUICryptoApp/Core/Detail/Views/DetailView.swift
@@ -39,26 +39,34 @@ struct DetailView: View {
     
     var body: some View {
         ScrollView {
-            VStack(spacing: 20) {
-                Text("")
-                    .frame(height: 150)
-                overviewTitle
-                
-                Divider()
-                
-                overviewGrid
-                
-                additionalTitle
-                
-                Divider()
-                
-                additionalGrid
+            VStack {
+                ChartView(coin: vm.coin)
+                    .padding(.vertical)
+                VStack(spacing: 20) {
+                    
+                    overviewTitle
+                    
+                    Divider()
+                    
+                    overviewGrid
+                    
+                    additionalTitle
+                    
+                    Divider()
+                    
+                    additionalGrid
+                }
+                .padding()
             }
-
-            .padding()
+        
            
         }
         .navigationTitle(vm.coin.name)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                navigationBarTrailingItems
+            }
+        }
     }
        
 }
@@ -72,6 +80,18 @@ struct DetailView: View {
 
 
 extension DetailView {
+    
+    private var navigationBarTrailingItems: some View 
+    {
+        HStack {
+            Text(vm.coin.symbol.uppercased())
+                .font(.headline)
+                .foregroundStyle(Color.theme.secondaryText)
+
+            CoinImageView(coin: vm.coin)
+                .frame(width: 25,height: 25)
+        }
+    }
     
     private var overviewTitle: some View {
         Text("Overview")
